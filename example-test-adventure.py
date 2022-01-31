261abb83bda3bab8fd537a092cf458196c1d9131
besucher <besucher@local>
2022-01-31 19:00:50 +0100

    it works. Now beta-testing!

--- example-test-adventure.py
+++ example-test-adventure.py
@@ -5,6 +5,8 @@
 import types
 import time
 
+end = False
+
 # Create a place.
 class Place():
 
@@ -37,7 +39,7 @@ class Engine():
 
         # How to navigate
         def choose_direction():
-            global task_done, task_todo
+            global task_done, task_todo, end
             directions = self.current_place.directions
             print("\nYou can leave to: ")
             for i in range(1, (len(directions)+1)):
@@ -47,9 +49,8 @@ class Engine():
             if e == "s":
                 return
             elif e == "e":
-                print("\nYou managed " + str(len(task_done)) + " of " + str(len(task_todo)) + " tasks.")
-                print("\nSee you next time!")
-                quit()
+                end = True
+                return
             try:
                 e = int(e)
                 if e > 0 and e <= len(directions):
@@ -134,7 +135,8 @@ class Engine():
                 return
 
         # The main menue
-        while 1 == 1:
+        global end
+        while end == False:
             print("------------------------------------------------------------------------")
             print("\n" + self.current_place.name + " - " + self.current_place.description)
             e = input("Do you want to look [a]round, to view your [i]tems or to [l]eave? ")
@@ -158,7 +160,7 @@ item = {}
 
 # Some examples of itemes.
 # Eating a banana is minus one.
-item["Banana"] = 2
+item["Banana"] = 1
 # After using a knive, it still exist.
 item["Knive"] = True
 
@@ -166,16 +168,15 @@ item["Knive"] = True
 # An example of crafting rules:
 # If you craft a banana with milk, you get a shake! Or you can slice an apple.
 craft = [
-        ["Cat food", "Knive", "Open-cat-food"],
-        ["Open-cat-food", "Feeding dish", "Lucky-cat"],
-        ["Apple", "Knive", "Slices"]
+        ["Cats food", "Knive", "Open cats food bag"],
+        ["Open cats food bag", "Feeding dish", "Full feeding dish"],
         ]
 
 # May you have to do a task_done to go further. Use as "global" in the beginnning of a function.
 task_done = []
 
 # Overview about tasks to do
-task_todo = ["relax", "art_expert", "eat_a_banana", "watering_flowers", "feed-the-cat"]
+task_todo = ["relax", "art_expert", "eat_a_banana", "watering_flowers", "feed_the_cat"]
 
 #
 # Working with a list in a list. Be carefull with counting the indices!
@@ -193,15 +194,20 @@ place[0].object.append(Object(name = "Picture"))
 
 # Create a function for the object[0] with the same name as above
 def picture(self):
-    print("\nWhat a wondefull picture!")
-    e = input("Do you [l]ike it? ")
-    if e == "l":
-        print("You are an expert!")
-        task_done.append("art_expert")
+    global task_done
+    if "art_expert" in task_done:
+        print("\nYou know this picture, you little expert.")
         return
     else:
-        print("You should better go to school again...")
-        return
+        print("\nWhat a wondefull picture!")
+        e = input("Do you [l]ike it? ")
+        if e == "l":
+            print("You are an expert!")
+            task_done.append("art_expert")
+            return
+        else:
+            print("May you change your judgement later...")
+            return
 
 # Bind the function to the object[0]. Care about the indices! In this case zero.
 place[0].object[0].start = types.MethodType(picture, place[0].object[0])
@@ -213,10 +219,7 @@ place[0].object.append(Object(name = "Door"))
 def door(self):
     global item, task_done
     print("\nThe door is locked.")
-    if not "Key" in item:
-        print("Sorry...")
-        return
-    elif "Key" in item:
+    if "Key" in item:
         e = input("But you have a key in your pocket. Do you want to [u]se it? ")
         if e == "u":
             print("\nGratulation! Now you can go outside.")
@@ -226,13 +229,19 @@ def door(self):
             return
         else:
             return
+    else:
+        print("Sorry...")
+        return
 
 # Bind the function to the new object[1] with the same name as above and the new index[1]!
 place[0].object[1].start = types.MethodType(door, place[0].object[1])
 
-# Create another place
+
+# Create a living room
 place.append(Place(name = "Living Room", directions = [0, 3]))
 place[1].set_description("Feel like home")
+
+# Add a table to the living room
 place[1].object.append(Object(name = "Table"))
 
 # Example to use the task_done and task_todo lists
@@ -257,28 +266,61 @@ def table(self):
             print("\nYou are very nervous. May relax a little bit?")
 
 place[1].object[0].start = types.MethodType(table, place[1].object[0])
+
+# Add a sofa to the living room
 place[1].object.append(Object(name = "Sofa"))
+
 def sofa(self):
     global item, task_done
-    e = input("\nWhat a nice Sofa. Do you want to [s]it down? ")
-    if e == "s":
-        print("Great! You relax a moment...")
-        # More fun with time
+    if not "relax" in task_done:
+        e = input("\nWhat a nice Sofa. Do you want to [s]it down? ")
+        if e == "s":
+            print("Great! You relax a moment...")
+            # More fun with time
+            for i in range(0, 4):
+                print(".")
+                time.sleep(1)
+            print("Ahh! It is good to take a rest.")
+            print("But wait... Oh f**k! You forgot to feed the cat.")
+            task_done.append("relax")
+            return
+        else:
+            print("You are a little bit nervous, right?")
+            return
+    else:
+        print("\n No time to relax. I have to do somethig... But what?")
+        return
+
+place[1].object[1].start = types.MethodType(sofa, place[1].object[1])
+
+# Add a cat to the living room
+place[1].object.append(Object(name = "Cat"))
+
+def cat(self):
+    global item, task_done, task_todo, end
+    print("\nHow sweet! Your cat is so lovely.")
+    if "Full feeding dish" in item:
+        print("Now you can feed the cat.")
+        print("You go to your sofa, sit down. Relax again.")
+        print("What a wonderfull day.")
+        task_done.append("feed_the_cat")
         for i in range(0, 4):
             print(".")
             time.sleep(1)
-        print("Ahh! It is good to take a rest.")
-        print("But wait... Oh f**k! You forgot to feed the cat.")
-        task_done.append("relax")
+        # Add the end
+        end = True
         return
     else:
-        print("You are a little bit nervous, right?")
+        print("Your cat run around you. Meow! May she is hungry?")
         return
-place[1].object[1].start = types.MethodType(sofa, place[1].object[1])
 
-# Create another place with all stuff
+place[1].object[2].start = types.MethodType(cat, place[1].object[2])
+
+# Create a kitchen
 place.append(Place(name = "Kitchen", directions = [0]))
 place[2].set_description("A little bit dirty")
+
+# Add a dustbin to the kitchen
 place[2].object.append(Object(name = "Dustbin"))
 
 def dustbin(self):
@@ -301,11 +343,12 @@ def dustbin(self):
 
 place[2].object[0].start = types.MethodType(dustbin, place[2].object[0])
 
+# Add a sink to the kitchen
 place[2].object.append(Object(name = "Sink"))
 
 def sink(self):
     global item, task_done
-    if "watering_flowers" not in task:
+    if "watering_flowers" not in task_done:
         e = input("\nNear the sink is a full watering can. Do you [t]ake it? ")
         if e == "t":
             item["Watering can"] = True
@@ -320,36 +363,38 @@ def sink(self):
 
 place[2].object[1].start = types.MethodType(sink, place[2].object[1])
 
-# Eat an apple
-def kitchen_table(self):
-    global item, task_done
-    pass
+# Add a cupboard to the Kitchen
+place[2].object.append(Object(name = "Cupboard"))
 
-# Take cat's food
 def kitchen_cupboard(self):
     global item, task_done
-    if "Cat food" in item:
+    if "Cats food" in item:
         print("\nThe cupboard is empty.")
         return
     else:
-        print("On the cupboard you find some cats food.")
-        e = input(" Do you [t]ake it? ")
+        print("\nOn the cupboard you find some cats food.")
+        e = input("Do you [t]ake it? ")
         if e == "t":
-            print("You grab the cats food.")
-            item["Cat food"] = 1
+            print("You grab the cats food. Mmmhh... How can I open it?")
+            item["Cats food"] = 1
             return
         else:
             print("You leave the cats food on the cupboard.")
             return
 
-# Create another place with all stuff
+place[2].object[2].start = types.MethodType(kitchen_cupboard, place[2].object[2])
+
+
+# Create a balcony
 place.append(Place(name = "Balcony", directions = [1]))
 place[3].set_description("What a wonderfull place!")
+
+# Add a flowerpot to the balcony
 place[3].object.append(Object(name = "Flowerpot"))
 
 def flowerpot(self):
     global item, task_done
-    if "watering_flowers" in task:
+    if "watering_flowers" in task_done:
         print("\nThe flowers are so lovly... Nice!")
     else:
         e = input("\nThe flowers are toasted. May you [w]ater them? ")
@@ -366,9 +411,32 @@ def flowerpot(self):
 
 place[3].object[0].start = types.MethodType(flowerpot, place[3].object[0])
 
-# Create another place with all stuff
+item["Key"] = True
+# Create a garden
 place.append(Place(name = "Garden", directions = [0]))
 place[4].set_description("Flowers and trees are around.")
+
+# Add a terrace
+place[4].object.append(Object(name = "Terrace"))
+
+#item["Feeding dish"] = True
+def terrace(self):
+    global item, task_done
+    if ("Feeding dish" or "Full feeding dish") in item:
+        print("\nThe terrace is tidy. But may you forgot something...")
+        return
+    else:
+        print("\nAt the corner of the terrace you find a feeding dish.")
+        e = input("Do you [t]ake it? ")
+        if e == "t":
+            print("You grab the feeding dish.")
+            item["Feeding dish"] = True
+            return
+        else:
+            print("You leave the feeding dish at the corner.")
+            return
+
+place[4].object[0].start = types.MethodType(terrace, place[3].object[0])
 
 # Print some prologe:
 print("\nWellcome to the poor ones textadventure engine.")
@@ -379,3 +447,7 @@ print("If you only press ENTER, you go back.")
 # Start the game!
 game = Engine()
 game.play()
+
+# Informations after theend of game
+print("\nYou solved " + str(len(task_done)) + " of " + str(len(task_todo)) + " tasks.")
+print("\nSee you next time!\n")
